284e1348110e4f041bc76a0f15607895fab062fb
mo3bius <ruben.sierra03@gmail.com>
2022-11-25 08:44:24 +0100

    exercise 3

---
+++ ejercicio 3.py
@@ -0,0 +1,17 @@
+from tkinter import *
+
+v0 = Tk()
+
+list1 = Listbox(v0)
+list1.pack()
+mivalor = StringVar()
+e1 = Entry(v0,textvar = mivalor).pack()
+
+def insertar_en_listbox():
+    if mivalor.get() != "":
+        list1.insert(END,mivalor.get())
+    else: print ("Por favor, escriba algun texto")
+
+b2 = Button(v0,text = "Insertar en Listbox", command = insertar_en_listbox).pack()
+
+v0.mainloop()
